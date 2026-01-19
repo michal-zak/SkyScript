66c60ec29fb1208aff583122865aca17c917121a
michal-zak <michalp752@gmail.com>
2026-01-19 10:45:28 +0100

    Initial Commit

---
+++ SkyScript/SkyScriptApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyScriptApp.swift
+//  SkyScript
+//
+//  Created by user945522 on 1/19/26.
+//
+
+import SwiftUI
+
+@main
+struct SkyScriptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SkyScriptTests/SkyScriptTests.swift
@@ -0,0 +1,16 @@
+//
+//  SkyScriptTests.swift
+//  SkyScriptTests
+//
+//  Created by user945522 on 1/19/26.
+//
+
+import Testing
+
+struct SkyScriptTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
