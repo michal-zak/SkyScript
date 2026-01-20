45470f80e7872c9b99ef0296a206ad1056740dde
michal-zak <michalp752@gmail.com>
2026-01-20 19:20:04 +0100

    Add AppIcon and screenShots

--- SkyScript/App/SkyScriptApp.swift
+++ SkyScript/App/SkyScriptApp.swift
@@ -2,7 +2,7 @@
 //  SkyScriptApp.swift
 //  SkyScript
 //
-//  Created by user945522 on 1/19/26.
+//  Created by michal-zak on 1/19/26.
 //
 
 import SwiftUI
--- SkyScript/Views/DashboardView.swift
+++ SkyScript/Views/DashboardView.swift
@@ -2,7 +2,7 @@
 //  Dashboardview.swift
 //  SkyScript
 //
-//  Created by user945522 on 1/19/26.
+//  Created by michal-zak on 1/19/26.
 //
 
 import SwiftUI
